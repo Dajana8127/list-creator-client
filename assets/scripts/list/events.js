e6d88e2a57fd5cd48190a9d112094576ea9e55e3
Dajana8127 <dajana.stankovic94@gmail.com>
2020-06-29 15:08:43 -0400

    Form fields are displayed in the browser

--- assets/scripts/list/events.js
+++ assets/scripts/list/events.js
@@ -6,11 +6,17 @@ const getFormFields = require('../../../lib/get-form-fields.js')
 const onListCreate = function (event) {
   // prevent
   event.preventDefault()
+  const title = $('#title').val()
+  $('#display-title').html(title)
+
   const form = event.target
   const data = getFormFields(form)
   api.listCreate(data)
     .then(ui.createSuccess)
     .catch(ui.createFailure)
+
+  const description = $('#description').val()
+  $('#display-description').html(description)
 }
 
 module.exports = {
